5612e2af23716834d0194969e523d7ec1197543a
Alexandre Baron <baron.alx@gmail.com>
2020-02-26 11:50:04 -0500

    Use plain char* for window titles for now

--- source/Window/WindowDescriptor.cpp
+++ source/Window/WindowDescriptor.cpp
@@ -3,7 +3,7 @@
 #include "WindowDescriptor.h"
 
 
-moe::WindowDescriptor::WindowDescriptor(Width_t w, Height_t h, const wchar_t * windowTitle, const char * iconPath) :
+moe::WindowDescriptor::WindowDescriptor(Width_t w, Height_t h, const char * windowTitle, const char * iconPath) :
 	m_width(w), m_height(h), m_title(windowTitle), m_iconPath(iconPath)
 {
 
--- source/Window/WindowDescriptor.h
+++ source/Window/WindowDescriptor.h
@@ -3,23 +3,18 @@
 #pragma once
 
 #include "Core/Misc/Types.h"
-#include "Core/Misc/moeNamedType.h"
+#include "Core/Misc/Literals.h" // width and height
 
 namespace moe
 {
 	struct WindowDescriptor
 	{
-		struct WidthParam {};
-		using Width_t = NamedType<uint32_t, WidthParam>;
 
-		struct HeightParam {};
-		using Height_t = NamedType<uint32_t, HeightParam>;
-
-		WindowDescriptor(Width_t w, Height_t h, const wchar_t* windowTitle, const char* iconPath);
+		WindowDescriptor(Width_t w, Height_t h, const char* windowTitle = "Monocle Application", const char* iconPath = nullptr);
 
 		Width_t				m_width;
 		Height_t			m_height;
-		const wchar_t*      m_title = nullptr;
+		const char*			m_title = nullptr;
 		const char*			m_iconPath = nullptr;
 
 	};
