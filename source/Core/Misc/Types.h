05c766c5e5c387a34b47e3dd70a62d17e8e3a45f
Alexandre Baron <baron.alx@gmail.com>
2020-02-26 11:52:14 -0500

    Add pragma once to Types.h

--- source/Core/Misc/Types.h
+++ source/Core/Misc/Types.h
@@ -1,3 +1,7 @@
+// Monocle Game Engine source files - Alexandre Baron
+
+#pragma once
+
 #include <cstdint>
 
 namespace moe
@@ -6,7 +10,7 @@ namespace moe
 		using std::int16_t;
 		using std::int32_t;
 		using std::int64_t;
-		
+
 		using std::uint8_t;
 		using std::uint16_t;
 		using std::uint32_t;
