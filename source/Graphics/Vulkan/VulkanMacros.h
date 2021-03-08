fc78322da43b85fdca93745fa7191f67d520e113
Alexandre Baron <baron.alx@gmail.com>
2021-03-08 00:00:54 -0500

    Integrating VULKAN_HPP_DISPATCH_LOADER_DYNAMIC into VulkanMacros.h to avoid silly errors and weird crashes because of Vulkan HPP.
    vulkan.hpp should always be included via VulkanMacros.h now. Also fix a compile error on MOE_VK_CHECK macro.

--- source/Graphics/Vulkan/VulkanMacros.h
+++ source/Graphics/Vulkan/VulkanMacros.h
@@ -3,13 +3,16 @@
 #pragma once
 
 #include "Core/Preprocessor/moeAssert.h"
+
+// Activating the dynamic dispatcher in order to easily use Vulkan EXTension functions.
+#define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
 #include <vulkan/vulkan.hpp>
 
 #define MOE_VK_CHECK(op)						\
 	do											\
 	{											\
 		vk::Result err = op;					\
-		if (err)								\
+		if (err != vk::Result::eSuccess)		\
 		{										\
 			MOE_ERROR(moe::ChanGraphics,		\
 				"Unexpected Vulkan Error: %s",	\
@@ -27,6 +30,5 @@ namespace moe
 #endif
 
 }
-
 
 #endif // MOE_VULKAN
